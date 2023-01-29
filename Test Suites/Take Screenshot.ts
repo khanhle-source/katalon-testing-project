b1ede8cc4ea8c7797f2bf1e738ad6747a83b8fa3
Khanh Le <nhukhanhle@gmail.com>
2023-01-29 23:08:40 +0700

    screenshot commit

--- Test Suites/Take Screenshot.ts
+++ Test Suites/Take Screenshot.ts
@@ -5,10 +5,24 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>208a0065-74e9-49a0-ae5b-36b5e643306d</testSuiteGuid>
+   <testCaseLink>
+      <guid>859e8595-6590-4842-aa82-0675bf853b7f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Take screenshot as checkpoint in login page</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c5979362-a97c-4436-b4fb-55ef04bcaae1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Sucessful</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
