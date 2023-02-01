841445462191fbcea261807c477d4c7dd4e8bfa1
Khanh Le <nhukhanhle@gmail.com>
2023-02-01 17:47:47 +0700

    commit 1 feb

---
+++ Test Suites/Create Installation Record.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Create Installation Record</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f0bb160b-1c88-443b-b59b-b28af56d485b</testSuiteGuid>
+</TestSuiteEntity>
